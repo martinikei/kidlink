ca40d02f0795970c0ebbd5db7473ee7bf2612c2f
marcin <marcin@sls>
2022-11-20 13:31:57 +0100

    dev change

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,4 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+export NODE_OPTIONS=--openssl-legacy-provider
 
 const environment = require('./environment')
 
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,4 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+export NODE_OPTIONS=--openssl-legacy-provider
 
 const environment = require('./environment')
 
